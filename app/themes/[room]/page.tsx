a9c4db366f8dcf08f9d498d284a5f3d9d29b472e
janiperta-debug <jani.perta@gmail.com>
2026-09-14 12:30:10 +0300

    Use CSS module for Conservatory materials

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -1,4 +1,4 @@
-import "../conservatory-materials.css"
+import "../conservatory-materials.module.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
