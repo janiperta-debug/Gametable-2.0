edb53ba4444dc07f2437e370f32c7db214176a50
janiperta-debug <jani.perta@gmail.com>
2026-09-14 13:21:36 +0300

    Add Spa theme preview route

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -5,9 +5,10 @@ import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
 import { CONSERVATORY_THEME_PAGE } from "@/lib/conservatory-theme-page"
 import { FIRESIDE_LOUNGE_THEME_PAGE } from "@/lib/fireside-lounge-theme-page"
+import { SPA_THEME_PAGE } from "@/lib/spa-theme-page"
 
 export function generateStaticParams() {
-  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge"].map((room) => ({ room }))
+  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa"].map((room) => ({ room }))
 }
 
 export default async function RoomThemePageRoute({ params }: { params: Promise<{ room: string }> }) {
@@ -17,7 +18,9 @@ export default async function RoomThemePageRoute({ params }: { params: Promise<{
       ? CONSERVATORY_THEME_PAGE
       : room === "fireside-lounge"
         ? FIRESIDE_LOUNGE_THEME_PAGE
-        : getRoomThemePage(room)
+        : room === "spa"
+          ? SPA_THEME_PAGE
+          : getRoomThemePage(room)
   if (!data) {
     notFound()
   }
