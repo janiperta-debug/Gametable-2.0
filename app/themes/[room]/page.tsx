9fb26beb0b8b4e6f0674b587b1cf1bc0ddf9969a
janiperta-debug <jani.perta@gmail.com>
2026-09-14 21:18:58 +0300

    Wire Underground Temple room theme route

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -10,6 +10,7 @@ import "../clock-tower-materials.css"
 import "../dungeon-room-materials.css"
 import "../crystal-cavern-room-materials.css"
 import "../alchemist-laboratory-materials.css"
+import "../underground-temple-room-materials.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
@@ -27,9 +28,10 @@ import { CLOCK_TOWER_THEME_PAGE } from "@/lib/clock-tower-theme-page"
 import { DUNGEON_ROOM_THEME_PAGE } from "@/lib/dungeon-room-theme-page"
 import { CRYSTAL_CAVERN_ROOM_THEME_PAGE } from "@/lib/crystal-cavern-room-theme-page"
 import { ALCHEMIST_LABORATORY_THEME_PAGE } from "@/lib/alchemist-laboratory-theme-page"
+import { UNDERGROUND_TEMPLE_ROOM_THEME_PAGE } from "@/lib/underground-temple-room-theme-page"
 
 export function generateStaticParams() {
-  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery", "artroom", "ballroom", "map-room", "theater-room", "war-room", "clock-tower", "dungeon", "crystal-cavern", "alchemist-laboratory"].map((room) => ({ room }))
+  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery", "artroom", "ballroom", "map-room", "theater-room", "war-room", "clock-tower", "dungeon", "crystal-cavern", "alchemist-laboratory", "underground-temple"].map((room) => ({ room }))
 }
 
 export default async function RoomThemePageRoute({ params }: { params: Promise<{ room: string }> }) {
@@ -63,7 +65,9 @@ export default async function RoomThemePageRoute({ params }: { params: Promise<{
                               ? CRYSTAL_CAVERN_ROOM_THEME_PAGE
                               : room === "alchemist-laboratory"
                                 ? ALCHEMIST_LABORATORY_THEME_PAGE
-                                : getRoomThemePage(room)
+                                : room === "underground-temple"
+                                  ? UNDERGROUND_TEMPLE_ROOM_THEME_PAGE
+                                  : getRoomThemePage(room)
   if (!data) notFound()
   return <RoomThemeTemplate data={data} />
 }
