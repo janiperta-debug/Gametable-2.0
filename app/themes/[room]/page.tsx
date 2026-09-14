2d14f05fbebccd090ba9bd09e0d2f5fbe1a08458
janiperta-debug <jani.perta@gmail.com>
2026-09-14 13:15:20 +0300

    Add Fireside Lounge theme preview route

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -4,14 +4,20 @@ import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
 import { CONSERVATORY_THEME_PAGE } from "@/lib/conservatory-theme-page"
+import { FIRESIDE_LOUNGE_THEME_PAGE } from "@/lib/fireside-lounge-theme-page"
 
 export function generateStaticParams() {
-  return [...Object.keys(ROOM_THEME_PAGES), "conservatory"].map((room) => ({ room }))
+  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge"].map((room) => ({ room }))
 }
 
 export default async function RoomThemePageRoute({ params }: { params: Promise<{ room: string }> }) {
   const { room } = await params
-  const data = room === "conservatory" ? CONSERVATORY_THEME_PAGE : getRoomThemePage(room)
+  const data =
+    room === "conservatory"
+      ? CONSERVATORY_THEME_PAGE
+      : room === "fireside-lounge"
+        ? FIRESIDE_LOUNGE_THEME_PAGE
+        : getRoomThemePage(room)
   if (!data) {
     notFound()
   }
