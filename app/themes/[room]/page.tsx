dfb0f7899c5b523451552a8701452016ed99adb5
janiperta-debug <jani.perta@gmail.com>
2026-09-14 15:06:31 +0300

    Wire Art Room preview route

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -4,6 +4,7 @@ import "../gallery-materials.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
+import { ART_ROOM_THEME_PAGE } from "@/lib/art-room-theme-page"
 import { CONSERVATORY_THEME_PAGE } from "@/lib/conservatory-theme-page"
 import { FIRESIDE_LOUNGE_THEME_PAGE } from "@/lib/fireside-lounge-theme-page"
 import { SPA_THEME_PAGE } from "@/lib/spa-theme-page"
@@ -11,23 +12,25 @@ import { BAR_THEME_PAGE } from "@/lib/bar-theme-page"
 import { GALLERY_THEME_PAGE } from "@/lib/gallery-theme-page"
 
 export function generateStaticParams() {
-  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery"].map((room) => ({ room }))
+  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery", "artroom"].map((room) => ({ room }))
 }
 
 export default async function RoomThemePageRoute({ params }: { params: Promise<{ room: string }> }) {
   const { room } = await params
   const data =
-    room === "conservatory"
-      ? CONSERVATORY_THEME_PAGE
-      : room === "fireside-lounge"
-        ? FIRESIDE_LOUNGE_THEME_PAGE
-        : room === "spa"
-          ? SPA_THEME_PAGE
-          : room === "bar"
-            ? BAR_THEME_PAGE
-            : room === "gallery"
-              ? GALLERY_THEME_PAGE
-              : getRoomThemePage(room)
+    room === "artroom"
+      ? ART_ROOM_THEME_PAGE
+      : room === "conservatory"
+        ? CONSERVATORY_THEME_PAGE
+        : room === "fireside-lounge"
+          ? FIRESIDE_LOUNGE_THEME_PAGE
+          : room === "spa"
+            ? SPA_THEME_PAGE
+            : room === "bar"
+              ? BAR_THEME_PAGE
+              : room === "gallery"
+                ? GALLERY_THEME_PAGE
+                : getRoomThemePage(room)
   if (!data) {
     notFound()
   }
