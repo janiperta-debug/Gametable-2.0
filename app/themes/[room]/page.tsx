730b2275e9a3a05572ed03c5c006f689d94b3a26
janiperta-debug <jani.perta@gmail.com>
2026-09-14 12:38:47 +0300

    Apply themed backgrounds to all room theme pages

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -1,4 +1,5 @@
 import "../conservatory-materials.module.css"
+import "../theme-page-background.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
