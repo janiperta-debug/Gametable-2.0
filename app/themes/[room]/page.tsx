6f4c941654f523188c480121063b32cf1deb79ea
janiperta-debug <jani.perta@gmail.com>
2026-09-14 20:38:19 +0300

    Wire War Room preview route and materials

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -5,6 +5,7 @@ import "../artroom-materials.css"
 import "../ballroom-materials.css"
 import "../map-room-materials.css"
 import "../theater-room-materials.css"
+import "../war-room-materials.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
@@ -17,9 +18,10 @@ import { GALLERY_THEME_PAGE } from "@/lib/gallery-theme-page"
 import { BALLROOM_THEME_PAGE } from "@/lib/ballroom-theme-page"
 import { MAP_ROOM_THEME_PAGE } from "@/lib/map-room-theme-page"
 import { THEATER_ROOM_THEME_PAGE } from "@/lib/theater-room-theme-page"
+import { WAR_ROOM_THEME_PAGE } from "@/lib/war-room-theme-page"
 
 export function generateStaticParams() {
-  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery", "artroom", "ballroom", "map-room", "theater-room"].map((room) => ({ room }))
+  return [...Object.keys(ROOM_THEME_PAGES), "conservatory", "fireside-lounge", "spa", "bar", "gallery", "artroom", "ballroom", "map-room", "theater-room", "war-room"].map((room) => ({ room }))
 }
 
 export default async function RoomThemePageRoute({ params }: { params: Promise<{ room: string }> }) {
@@ -43,7 +45,9 @@ export default async function RoomThemePageRoute({ params }: { params: Promise<{
                     ? MAP_ROOM_THEME_PAGE
                     : room === "theater-room"
                       ? THEATER_ROOM_THEME_PAGE
-                      : getRoomThemePage(room)
+                      : room === "war-room"
+                        ? WAR_ROOM_THEME_PAGE
+                        : getRoomThemePage(room)
   if (!data) {
     notFound()
   }
