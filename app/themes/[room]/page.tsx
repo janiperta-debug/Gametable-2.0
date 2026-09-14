cb53d159a388e596dafd13c3cd6e1a556786433e
janiperta-debug <jani.perta@gmail.com>
2026-09-14 16:17:30 +0300

    Apply Art Room materials and colors to theme route

--- app/themes/[room]/page.tsx
+++ app/themes/[room]/page.tsx
@@ -1,6 +1,7 @@
 import "../conservatory-materials.module.css"
 import "../theme-page-background.css"
 import "../gallery-materials.css"
+import "../artroom-materials.css"
 import { notFound } from "next/navigation"
 import { RoomThemeTemplate } from "@/components/room-theme-template"
 import { getRoomThemePage, ROOM_THEME_PAGES } from "@/lib/room-theme-pages"
