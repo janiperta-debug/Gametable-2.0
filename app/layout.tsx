12f74f754a4a1305b72e88ec5f73f5f802b3ecfe
janiperta-debug <jani.perta@gmail.com>
2026-09-16 10:59:41 +0300

    Use non-translucent iOS PWA status bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
-    statusBarStyle: "black-translucent",
+    statusBarStyle: "default",
     title: "GameTable",
   },
   formatDetection: {
