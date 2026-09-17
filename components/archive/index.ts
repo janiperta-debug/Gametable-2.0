ee63ca33f529242985c4f1aa7f7e0c5317cb8ecf
janiperta-debug <jani.perta@gmail.com>
2026-09-17 16:14:13 +0300

    feat: export Archive UI component family

---
+++ components/archive/index.ts
@@ -0,0 +1,18 @@
+export {
+  ArchiveFrame,
+  ArchiveButton,
+  ArchiveCardButton,
+  ArchiveIconButton,
+  ArchiveToggle,
+  ArchiveCard,
+  ArchiveCardHeader,
+  ArchiveCardTitle,
+  ArchiveCardContent,
+  archiveField,
+  archiveSelectContent,
+  archiveSelectItem,
+} from "../archive-frame"
+export { ArchivePanel } from "../archive-panel"
+export { ArchiveTile } from "../archive-tile"
+export { ArchiveAvatarFrame } from "../archive-avatar-frame"
+export { ArchiveDivider } from "../archive-divider"
