23d2c0424715fd1c2faefa1ce9693c2deb7f0fdc
janiperta-debug <jani.perta@gmail.com>
2026-09-20 19:51:06 +0300

    fix: use direct official miniature image URLs

---
+++ supabase/migrations/20260920142000_seed_direct_miniature_image_urls.sql
@@ -0,0 +1,11 @@
+update public.mini_units
+set
+  image_url = 'https://cdn.svc.asmodee.net/production-amgcom/uploads/image-converter/2022/06/CP37.webp',
+  image_source_url = 'https://www.atomicmassgames.com/character/amazing-spider-man-black-cat/'
+where id = 'ae1f83bf-600c-4a6f-9cbc-d0dbae02424b';
+
+update public.mini_units
+set
+  image_url = 'https://assets.warhammer-community.com/articles/60628fab-8cc0-4d8a-af66-a995f19bc198/ci1p3nfpg6ymcphd.jpg',
+  image_source_url = 'https://www.warhammer-community.com/en-gb/articles/2u4kOYKl/more-toughness-more-wounds-bigger-guns-heavy-intercessors-have-it-all/'
+where id = '4208c209-1a58-47f3-9268-aa39edcf8ca8';
