2f98964c86c2135a87ffbd22c9469e2540a5c769
janiperta-debug <jani.perta@gmail.com>
2026-09-20 20:13:51 +0300

    fix: seed direct miniature image urls

---
+++ supabase/migrations/20260920140000_seed_direct_miniature_image_urls.sql
@@ -0,0 +1,7 @@
+update public.mini_units
+set image_url = 'https://cdn.svc.asmodee.net/production-amgcom/uploads/image-converter/2022/06/CP37.webp'
+where id = 'ae1f83bf-600c-4a6f-9cbc-d0dbae02424b';
+
+update public.mini_units
+set image_url = 'https://assets.warhammer-community.com/articles/60628fab-8cc0-4d8a-af66-a995f19bc198/ci1p3nfpg6ymcphd.jpg'
+where id = '4208c209-1a58-47f3-9268-aa39edcf8ca8';
