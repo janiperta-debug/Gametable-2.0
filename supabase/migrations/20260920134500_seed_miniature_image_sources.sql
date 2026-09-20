fe519b260346488420d586a5085a251d136a5de1
janiperta-debug <jani.perta@gmail.com>
2026-09-20 15:09:14 +0300

    feat: seed MCP and Warhammer image sources

---
+++ supabase/migrations/20260920134500_seed_miniature_image_sources.sql
@@ -0,0 +1,7 @@
+update public.mini_units
+set image_source_url = 'https://www.atomicmassgames.com/character/amazing-spider-man-black-cat/'
+where id = 'ae1f83bf-600c-4a6f-9cbc-d0dbae02424b';
+
+update public.mini_units
+set image_source_url = 'https://www.warhammer-community.com/en-gb/articles/2u4kOYKl/more-toughness-more-wounds-bigger-guns-heavy-intercessors-have-it-all/'
+where id = '4208c209-1a58-47f3-9268-aa39edcf8ca8';
