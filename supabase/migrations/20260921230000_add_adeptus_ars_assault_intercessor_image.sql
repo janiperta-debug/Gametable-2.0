b58644b79b0e72979813b5167951dc90305018f4
janiperta-debug <jani.perta@gmail.com>
2026-09-21 06:12:19 +0300

    data: add Adeptus Ars Assault Intercessor image

---
+++ supabase/migrations/20260921230000_add_adeptus_ars_assault_intercessor_image.sql
@@ -0,0 +1,5 @@
+update mini_units
+set image_url = 'https://www.adeptusars.com/miniatures/assault-intercessors',
+    image_source_url = 'https://www.adeptusars.com/miniatures/assault-intercessors'
+where id = '7a0192b0-a5b2-4e9f-a308-cc1a62963317'
+  and name = 'Assault Intercessor Squad';
