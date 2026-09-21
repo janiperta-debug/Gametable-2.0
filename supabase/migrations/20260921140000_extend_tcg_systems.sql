0ae64736661b52f3be799def0572846471c989a1
janiperta-debug <jani.perta@gmail.com>
2026-09-21 15:03:11 +0300

    Allow all supported TCG systems in database

---
+++ supabase/migrations/20260921140000_extend_tcg_systems.sql
@@ -0,0 +1,13 @@
+alter table public.tcg_cards
+  drop constraint if exists tcg_cards_tcg_system_check;
+
+alter table public.tcg_cards
+  add constraint tcg_cards_tcg_system_check
+  check (tcg_system = any (array[
+    'mtg'::text,
+    'pokemon'::text,
+    'yugioh'::text,
+    'lorcana'::text,
+    'flesh-and-blood'::text,
+    'one-piece'::text
+  ]));
