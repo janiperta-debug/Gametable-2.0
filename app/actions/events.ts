823c91f2d40081862f1f6d4ad94b79646026fb78
janiperta-debug <jani.perta@gmail.com>
2026-09-23 17:17:28 +0300

    Expose past events action

--- app/actions/events.ts
+++ app/actions/events.ts
@@ -1,5 +1,5 @@
-import { getPublicEvents, getMyEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent } from "./events-core"
+import { getPublicEvents, getMyEvents, getPastEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent } from "./events-core"
 import { inviteToEvent } from "./event-invitations"
 
-export { getPublicEvents, getMyEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent, inviteToEvent }
+export { getPublicEvents, getMyEvents, getPastEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent, inviteToEvent }
 export type { EventType, EventPrivacy, EventStatus, RSVPStatus, Event, EventParticipant, EventMessage } from "./events-core"
