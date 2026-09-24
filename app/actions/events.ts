de3e3bc0aaaafe7825ca53fbeb319b8fbcbb63d8
janiperta-debug <jani.perta@gmail.com>
2026-09-24 06:31:57 +0300

    Export event completion action

--- app/actions/events.ts
+++ app/actions/events.ts
@@ -1,4 +1,4 @@
-import { getPublicEvents, getMyEvents, getPastEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent } from "./events-core"
+import { getPublicEvents, getMyEvents, getPastEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent, completeEvent } from "./events-core"
 import { inviteToEvent } from "./event-invitations"
 
 export { getPublicEvents, getMyEvents, getPastEvents, createEvent, updateRSVP, getEventParticipants, getEventById, updateEvent, getEventMessages, sendEventMessage, uninviteFromEvent, getInvitableUsers, cancelEvent, inviteToEvent }
